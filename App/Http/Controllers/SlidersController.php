d91d01b0b47a0da8ee45bbead99c39914c905db2
KassieSanisclaws <86675041+KassieSanisclaws@users.noreply.github.com>
2021-07-15 04:29:08 -0700

    Add files via upload

---
+++ App/Http/Controllers/SlidersController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Slider;
+
+class SlidersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sliders = Slider::get();
+        return view('admin.slider.index', compact('sliders'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.slider.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, ['image'=>'required|mimes:png,jpg']);
+        $image = $request->file('image')->store('public/slider');
+        Slider::create(['image'=>$image]);
+
+        // notify()->success('Image UpLoaded Successfully!');
+        return redirect()->back()->with('message', 'Slider Created Successfully!.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $slider = Slider::find($id);
+        $slider->delete();
+       // notify()->success('Image Deleted Successfully!');
+        return redirect()->back()->with('message', 'Slider Image Deleted Successfully!.');
+    }
+}
